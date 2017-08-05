169d5d011549320d5c69cee64398e9848dde6d7f
Peter Ossian <msuossianpe@gmail.com>
2017-08-05 15:43:18 -0700

    added gcc compilation commands

--- lib/balongdiffer.rb
+++ lib/balongdiffer.rb
@@ -7,8 +7,18 @@ end
 
 class Balongdiffer::Balongnvtool
   def initialize
-    cmd = Gem.loaded_specs['balongdiffer'].full_gem_path + "/ext/balong-nvtool/make"
-    value = `#{cmd}`
+    unless File.exist?(Gem.loaded_specs['balongdiffer'].full_gem_path + "/ext/balong-nvtool/modem-bin/balong-nvtool")
+      build_cmd = ["gcc -O2  -Wunused -Wno-unused-result -D BUILDNO=129 -c -o balong-nvtool.o balong-nvtool.c",
+                   "gcc -O2  -Wunused -Wno-unused-result -D BUILDNO=129 -c -o nvio.o nvio.c",
+                   "gcc -O2  -Wunused -Wno-unused-result -D BUILDNO=129 -c -o nvid.o nvid.c",
+                   "gcc -O2  -Wunused -Wno-unused-result -D BUILDNO=129 -c -o sha2.o sha2.c",
+                   "gcc -O2  -Wunused -Wno-unused-result -D BUILDNO=129 -c -o nvcrc.o nvcrc.c",
+                   "gcc balong-nvtool.o nvio.o nvid.o sha2.o nvcrc.o -o balong-nvtool"]
+      Dir.chdir(Gem.loaded_specs['balongdiffer'].full_gem_path + "/ext/balong-nvtool")
+      build_cmd.each do |single_cmd|
+        `#{single_cmd}`
+      end
+    end
   end
   def printaddressnames(filename)
     if filename.to_s.empty?
